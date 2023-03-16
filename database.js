f77f80bd1bc0a8f4bfdb1485cd9e2505a76c7c8f
AmitGujar <amithero3342@gmail.com>
2023-03-17 04:45:56 +0530

    file added

---
+++ database.js
@@ -0,0 +1,12 @@
+const {createPool} = require("mysql");
+
+const pool = createPool ({
+    server:"10.0.0.4",
+    user:"amit",
+    password:"1234",
+    database:"appdb",
+});
+
+pool.query(`select * from Course`, (err, res) => {
+    return console.log(res);
+});
